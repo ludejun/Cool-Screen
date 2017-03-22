5f9578bf9e0a4e7be1f38a162e349029d56b202b
TianJie <tianjie12@wanda.cn>
2017-03-22 10:45:27 +0800

    locPop data update and bug fix

--- public/wanda/pplt_move/js/locPop.js
+++ public/wanda/pplt_move/js/locPop.js
@@ -31,76 +31,77 @@ option = {
         }
 
     }],
-    yAxis: [   
-                {
-                    type: "category",
-                    data: ["康馨苑", "泰苑公寓", "金源别墅区", "周浦镇", "惠南镇"],
-                    axisLine: {
-                        show: false
-                    },
-                    splitLine: {
-                        show: false
-                    },
-                    axisTick: {
-                        show: false
-                    }
-                }, {
-                    type: "category",
-                    data: [18203, 29034, 104970, 131744, 630230],
-                    axisLine: {
-                        show: false
-                    },
-                    splitLine: {
-                        show: false
-                    },
-                    axisTick: {
-                        show: false
-                    }
-                }
-            ],
-        grid: {
-            bottom: 18,
-            left: 80
-        },
-        textStyle: {
-            color: '#FFFFFF',
-            fontSize: 18
-        },
-        series: [
-                 {
-                      type: "bar",
-                      z: 10,
-                      tooltip:{
-                          show:false
-                      },
-                      data: [18203, 29034, 104970, 131744, 630230],
-                      itemStyle: {
-                          normal: {
-                              color: function(params) {
-                                  // build a color map as your need.
-                                  var colorList = [
-                                      '#C740F9','#FF81ED','#F9C840','#FFC3C3','#F99F40'
-                                  ];
-                                  return colorList[params.dataIndex]
-                              },
-                          }
-                      }
-                  },
-                  {
-                      type: "bar",
-                      silent: true,
-                      itemStyle: {
-                          normal: {
-                              color: 'rgba(255,255,255,0.39)',
-                          },
-                      },
-                      tooltip:{
-                          show:false
-                      },
-                      barGap: '-100%',
-                      data: [700000, 700000, 700000, 700000, 700000],
+    yAxis: [
+         {   
+             type: "category",
+             data: ["江湾", "延吉", "五角场", "中原", "曲阳"],
+             axisLine: {
+                 show: false
+             },
+             splitLine: {
+                 show: false
+             },
+             axisTick: {
+                 show: false
+             }
+         },
+         {
+             type: "category",
+             data: [177340, 164400, 114125, 109343, 130552],
+             axisLine: {
+                 show: false
+             },
+             splitLine: {
+                 show: false
+             },
+             axisTick: {
+                 show: false
+             }
+         }
+    ],
+    grid: {
+        bottom: 18,
+        left: 80
+    },
+    textStyle: {
+        color: '#FFFFFF',
+        fontSize: 18
+    },
+    series: [
+        {
+             type: "bar",
+             z: 10,
+             tooltip:{
+                 show:false
+             },
+             data: [177340, 164400, 114125, 109343, 130552],
+             itemStyle: {
+                 normal: {
+                     color: function(params) {
+                         // build a color map as your need.
+                         var colorList = [
+                             '#C740F9','#FF81ED','#F9C840','#FFC3C3','#F99F40'
+                         ];
+                         return colorList[params.dataIndex]
+                     },
                  }
-                ]
+             }
+        },
+        {
+             type: "bar",
+             silent: true,
+             itemStyle: {
+                 normal: {
+                     color: 'rgba(255,255,255,0.39)',
+                 },
+             },
+             tooltip:{
+                 show:false
+             },
+             barGap: '-100%',
+             data: [700000, 700000, 700000, 700000, 700000],
+        }
+    ]
 };
 
 var myChart = echarts.init(document.getElementById('chart5_ctn'));
