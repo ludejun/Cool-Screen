e27b21b69d649806cfbbf1924e4afd68805659d8
ludejun1 <ludejun1@wanda.cn>
2017-07-03 19:53:38 +0800

    home earth

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
+import echarts from 'echarts';
+import 'echarts-gl';
 import './home.less';
 
 export default class Home extends Component {
-  render () {
+
+  componentDidMount() {
+    const chart = echarts.init(document.getElementById('home-earth'));
+    chart.setOption({
+      backgroundColor: 'transparent',
+      globe: {
+        baseTexture: '/img/echarts/data-1491890179041-Hkj-elqpe.jpg',
+        displacementScale: 0.1,
+        shading: 'lambert',
+
+        light: {
+          ambient: {
+            intensity: 0.1
+          },
+          main: {
+            intensity: 1.5
+          }
+        },
+
+        layers: [{
+          type: 'blend',
+          blendTo: 'emission',
+          texture: '/img/echarts/data-1491890291849-rJ2uee5ag.jpg'
+        }, {
+          type: 'overlay',
+          texture: '/img/echarts/data-1491890092270-BJEhJg96l.png',
+          shading: 'lambert',
+          distance: 5
+        }]
+      },
+      series: [{
+        type: 'lines3D',
+        coordinateSystem: 'globe',
+        blendMode: 'lighter',
+        effect: {
+          show: true,
+        },
+        lineStyle: {
+          color: 'rgb(50, 50, 150)',
+          width: 3,
+          opacity: 0.8,
+        },
+        data: [[
+          [120, 66, 1], // 起点的经纬度和海拔坐标
+          [122, 32, 2]  // 终点的经纬度和海拔坐标
+        ], [
+          [120, 66, 1], [-80, 30, 1]
+        ],]
+      }]
+    })
+
+  }
+
+  render() {
     return (
       <div className="super-home">
         <img src="/img/home-title.png" className="home-title" />
