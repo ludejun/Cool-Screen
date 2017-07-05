7707ac10c3e535fc30404213bbe9ffb83e72c104
guming <1065316432@qq.com>
2017-07-05 15:41:42 +0800

    add WdNum component

---
+++ src/components/WDNum.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './WDNum.less';
+import anime from 'animejs';
+
+export default class WDNum extends Component {
+  render() {
+
+    return (
+      <div className="wdNum">
+      </div>
+    );
+  }
+}
