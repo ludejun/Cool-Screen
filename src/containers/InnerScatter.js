64052f1a3acda5d201964522b88dbaaeb45d6fc4
v_qinhaohao <qhh_javaee@sina.com>
2017-07-11 11:21:17 +0800

    修改button 样式

--- src/containers/InnerScatter.js
+++ src/containers/InnerScatter.js
@@ -17,12 +17,12 @@ export default class InnerScatter extends Component {
     return (
       <div className="inner-scatter-container">
           <div className="main-content">
-              <div className="China-map">map</div>
+              <div className="china-map">map</div>
               <div className="bar">bar</div>
           </div>
           <div className="slide-btn">
               <ul className="slide-wrap">
-                  <li>广场</li>
+                  <li><i className="left"></i>广场<i className="right"></i></li>
                   <li>城市</li>
                   <li className="active">省份</li>
                   <li>区域</li>
