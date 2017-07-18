3617a70c6d3e85d9ea3cbd130f983c156a4f30db
suga <suga_e@outlook.com>
2017-07-18 18:39:56 +0800

    add LifeOSUV tags;sort folder;

---
+++ src/containers/LifeOSUV/LifeOSUVTags.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+
+export default class LifeOSUVTags extends Component {
+  constructor() {
+    super();
+    this.typelist = {
+      0: {
+        stroke: '#2EF7FF',
+        fill: 'url(#tags-linearGradient-4)',
+        opacity: '0.540194746'
+      },
+      1: {
+        stroke: '#168EFF',
+        fill: 'url(#tags-linearGradient-3)',
+        opacity: '0.540194746'
+      },
+      2: {
+        stroke: '#108EE9',
+        strokeWidth: '1',
+        fill: 'black',
+        fillOpacity: '1',
+        filter: 'url(#tags-filter-2)'
+      }
+    };
+  }
+  render() {
+    return (
+      <svg
+        // width="188px"
+        // height="56px"
+        viewBox="0 0 188 56"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        {...this.props}
+      >
+        <defs>
+          <polygon
+            id="tags-path-1"
+            points="18.6931818 0 188 0 188 39.8644068 171.977273 56 0 56 0 14.2372881"
+          />
+          <filter
+            x="-5.9%"
+            y="-19.6%"
+            width="111.7%"
+            height="139.3%"
+            filterUnits="objectBoundingBox"
+            id="tags-filter-2"
+          >
+            <feGaussianBlur stdDeviation="11" in="SourceAlpha" result="shadowBlurInner1" />
+            <feOffset dx="0" dy="0" in="shadowBlurInner1" result="shadowOffsetInner1" />
+            <feComposite
+              in="shadowOffsetInner1"
+              in2="SourceAlpha"
+              operator="arithmetic"
+              k2="-1"
+              k3="1"
+              result="shadowInnerInner1"
+            />
+            <feColorMatrix
+              values="0 0 0 0 0   0 0 0 0 0.869055707   0 0 0 0 1  0 0 0 0.5 0"
+              type="matrix"
+              in="shadowInnerInner1"
+            />
+          </filter>
+          <linearGradient
+            x1="42.6479507%"
+            y1="-26.362553%"
+            x2="78.2587965%"
+            y2="117.349448%"
+            id="tags-linearGradient-3"
+          >
+            <stop stopColor="#00B6FF" offset="0%" />
+            <stop stopColor="#2D0094" stopOpacity="0.091796875" offset="100%" />
+          </linearGradient>
+          <linearGradient
+            x1="32.0846325%"
+            y1="-64.3980795%"
+            x2="78.2587965%"
+            y2="117.349448%"
+            id="tags-linearGradient-4"
+          >
+            <stop stopColor="#32ECD9" offset="0%" />
+            <stop stopColor="#1B861E" stopOpacity="0.091796875" offset="100%" />
+          </linearGradient>
+        </defs>
+
+        <g id="osuv-tags" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          {/*
+            <use
+              stroke="#108EE9"
+              strokeWidth="1"
+              fill="black"
+              fillOpacity="1"
+              filter="url(#tags-filter-2)"
+              xlinkHref="#tags-path-1"
+            />
+          */}
+          <use
+            // stroke="#168EFF"
+            // fill="url(#tags-linearGradient-3)"
+            // opacity="0.540194746"
+            xlinkHref="#tags-path-1"
+            {...this.typelist[this.props.type]}
+          />
+
+          <text
+            id="35%"
+            fontFamily="PingFangSC-Regular, PingFang SC"
+            fontSize="36"
+            fontWeight="normal"
+            fill={this.props.type === 2 ? '#30D3FE' : '#fff'}
+          >
+            <tspan x="22" y="38">{this.props.percent || '00'}</tspan>
+            <tspan x="65.2" y="38" fontSize="20">%</tspan>
+          </text>
+          <text
+            id="闪购购"
+            fontFamily="PingFangSC-Regular, PingFang SC"
+            fontSize="24"
+            fontWeight="normal"
+            fill={this.props.type === 2 ? '#30D3FE' : '#fff'}
+          >
+            <tspan x="99" y="37">{this.props.title || ''}</tspan>
+          </text>
+        </g>
+      </svg>
+    );
+  }
+}
---
+++ src/containers/LifeOSUV/index.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { WDButtonSvg } from '../../components';
+import HeaderTitle from '../Layout/HeaderTitle';
+import AppleLogo from './AppleLogo';
+import AndroidLogo from './AndroidLogo';
+import LifeOSUVDotBg from './LifeOSUVDotBg';
+import LifeOSUVRoundBoard from './LifeOSUVRoundBoard';
+import LifeOSUVTags from './LifeOSUVTags';
+
+import './LifeOSUV.less';
+import './LifeOSUVAnime.less';
+
+export default class LifeOSUV extends Component {
+  constructor() {
+    super();
+
+    //     1	ios	广场	27712	23.060855961188	46.121711922376
+    // 2	ios	优惠券	24214	20.1499554793666	40.2999109587331
+    // 3	ios	电影	20070	16.7014787507593	33.4029575015187
+    // 4	ios	闪购	16394	13.6424535445914	27.2849070891827
+    // 5	ios	门店	14985	12.46993817041	24.93987634082
+    // 6	ios	其它	16794	13.9753180936847	27.9506361873695
+    // 	ios	外圈sum	62767	52.2322728823573	104.464545764715
+    // 	ios	内圈sum	57402	47.7677271176427	95.5354542352853
+    // 		sum	120169		200
+
+    //     1	android	广场	52324	28.5563032456298	57.1126064912597
+    // 2	android	闪购	35292	19.2609329207394	38.5218658414788
+    // 3	android	优惠券	34085	18.6022015925253	37.2044031850506
+    // 4	android	门店	19276	10.5200539210068	21.0401078420136
+    // 5	android	电影	16334	8.91443041843356	17.8288608368671
+    // 6	android	其它	25920	14.1460779016651	28.2921558033302
+    // 	android	外圈sum	102743	56.0729352565887	112.145870513177
+    // 	android	内圈sum	80488	43.9270647434113	87.8541294868226
+    // 		sum	183231	100	200
+
+    this.list = {
+      ios: {
+        out: '46% 40% 34% 27% 25% 28%',
+        in: '33% 27% 22% 20.5% 22.5% 46%',
+        offset: '-37%',
+        tags: [
+          { title: '广场', percent: 23 },
+          { title: '优惠券', percent: 20 },
+          { title: '电影', percent: 17 },
+          { title: '闪购', percent: 14 },
+          { title: '门店', percent: 12 },
+          { title: '其它', percent: 14 }
+        ]
+      },
+      android: {
+        out: '57% 39% 37% 21% 18% 28%',
+        in: '32% 30% 17% 14.5% 22.5% 51%',
+        offset: '-46%',
+        tags: [
+          { title: '广场', percent: 29 },
+          { title: '闪购', percent: 19 },
+          { title: '优惠券', percent: 19 },
+          { title: '门店', percent: 11 },
+          { title: '电影', percent: 9 },
+          { title: '其它', percent: 13 }
+        ]
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="life-osuv flex-row flex-center dark-bg">
+        <HeaderTitle title="智慧生活：按照移动OS类型／应用UV对比" />
+        {Object.keys(this.list).map(v =>
+          <div className={`dotted-panel panel-${v} flex1`} key={v}>
+            <LifeOSUVDotBg className="dot-bg" />
+            <LifeOSUVRoundBoard
+              className="round-board"
+              outerDashArray={{ strokeDasharray: this.list[v].out }}
+              innerDashArray={{
+                strokeDasharray: this.list[v].in,
+                strokeDashoffset: this.list[v].offset
+              }}
+            />
+            {v === 'ios'
+              ? <AppleLogo className="center-logo" />
+              : <AndroidLogo className="center-logo" />}
+            <WDButtonSvg className="title-button" title={v} />
+            {this.list[v].tags.map((t, i) =>
+              <LifeOSUVTags key={i} className={`tags tags-${v}-${i}`} {...t} type={i % 3} />
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
