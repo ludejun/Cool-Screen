6c54f5e50eb3f501c50a64b348b15858ad8c954d
guming <1065316432@qq.com>
2017-07-26 14:15:07 +0800

    beautify file

--- src/containers/InnerScatter/InnerScatterLong.js
+++ src/containers/InnerScatter/InnerScatterLong.js
@@ -1,130 +1,126 @@
-import React, { Component } from "react";
-import Echarts from "echarts-for-react";
-import { WDMapBasic } from "../../components";
-import { plazaGeo } from "../../assets/map/wdplaza.geo";
-import { cityGeo } from "../../assets/map/city.geo";
-import {
-  provinceValue,
-  proArea,
-  areaValue
-} from "../../assets/map/mapAreaValue";
-import HeaderTitle from "../Layout/HeaderTitle";
-import { getBaseFontSize } from "../../utils";
-import "./innerScatterLong.less";
+import React, { Component } from 'react';
+import Echarts from 'echarts-for-react';
+import { WDMapBasic } from '../../components';
+import { plazaGeo } from '../../assets/map/wdplaza.geo';
+import { cityGeo } from '../../assets/map/city.geo';
+import { provinceValue, proArea, areaValue } from '../../assets/map/mapAreaValue';
+import HeaderTitle from '../Layout/HeaderTitle';
+import { getBaseFontSize } from '../../utils';
+import './innerScatterLong.less';
 
 const pillarData = [
   [
     {
-      name: "郑州惠济万达广场",
+      name: '郑州惠济万达广场',
       value: [113.640612525, 34.8684219261, 13478],
-      percent: "100%"
+      percent: '100%'
     },
     {
-      name: "徐州铜山万达广场",
+      name: '徐州铜山万达广场',
       value: [117.1902587966, 34.1960255201, 11544],
-      percent: "85.7%"
+      percent: '85.7%'
     },
     {
-      name: "北京槐房万达广场",
+      name: '北京槐房万达广场',
       value: [116.3682659401, 39.813840514, 11048],
-      percent: "82.0%"
+      percent: '82.0%'
     },
     {
-      name: "东莞虎门万达广场",
+      name: '东莞虎门万达广场',
       value: [113.682834261, 22.8330205339, 10984],
-      percent: "81.5%"
+      percent: '81.5%'
     },
     {
-      name: "烟台开发区万达广场",
+      name: '烟台开发区万达广场',
       value: [121.2565552877, 37.5375066159, 10266],
-      percent: "76.2%"
+      percent: '76.2%'
     }
   ],
   [
     {
-      name: "北京",
+      name: '北京',
       value: [116.395645038, 39.9299857781, 338700],
-      percent: "100%"
+      percent: '100%'
     },
     {
-      name: "郑州",
+      name: '郑州',
       value: [113.64964385, 34.7566100641, 234991],
-      percent: "69.4%"
+      percent: '69.4%'
     },
     {
-      name: "成都",
+      name: '成都',
       value: [104.067923463, 30.6799428454, 226535],
-      percent: "66.9%"
+      percent: '66.9%'
     },
     {
-      name: "徐州",
+      name: '徐州',
       value: [117.188106623, 34.2715534311, 195691],
-      percent: "57.8%"
+      percent: '57.8%'
     },
     {
-      name: "烟台",
+      name: '烟台',
       value: [121.30955503, 37.5365615629, 157102],
-      percent: "46.4%"
+      percent: '46.4%'
     }
   ],
   [
     {
-      name: "江苏",
+      name: '江苏',
       value: 36786,
-      percent: "100%"
+      percent: '100%'
     },
     {
-      name: "福建",
+      name: '福建',
       value: 26579,
-      percent: "72.3%"
+      percent: '72.3%'
     },
     {
-      name: "北京",
+      name: '北京',
       value: 20867,
-      percent: "56.7%"
+      percent: '56.7%'
     },
     {
-      name: "湖北",
+      name: '湖北',
       value: 18358,
-      percent: "49.9%"
+      percent: '49.9%'
     },
     {
-      name: "安徽",
+      name: '安徽',
       value: 17239,
-      percent: "46.9%"
+      percent: '46.9%'
     }
   ],
   [
     {
-      name: "华东区",
+      name: '华东区',
       value: 119331,
-      percent: "100%"
+      percent: '100%'
     },
     {
-      name: "中南区",
+      name: '中南区',
       value: 54783,
-      percent: "45.9%"
+      percent: '45.9%'
     },
     {
-      name: "东北区",
+      name: '东北区',
       value: 32421,
-      percent: "27.2%"
+      percent: '27.2%'
     },
     {
-      name: "华北区",
+      name: '华北区',
       value: 31823,
-      percent: "26.7%"
+      percent: '26.7%'
     },
     {
-      name: "西南区",
+      name: '西南区',
       value: 15345,
-      percent: "12.9%"
+      percent: '12.9%'
     }
   ]
 ];
 
 const pillar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const showType = ["广场", "城市", "省份", "区域"];
+const showType = ['广场', '城市', '省份', '区域'];
 export default class InnerScatterFour extends Component {
   constructor(props) {
     super(props);
@@ -154,62 +150,62 @@ export default class InnerScatterFour extends Component {
     const topGeo = index === 0 ? pillarData[0] : pillarData[1];
     const geo = index === 0 ? plazaGeo : cityGeo;
     const multiple = index === 0 ? 1500 : 15000;
-    const color = "#108EE9";
+    const color = '#108EE9';
     const scatterMap = {
       series: [
         {
-          type: "effectScatter",
-          coordinateSystem: "geo",
+          type: 'effectScatter',
+          coordinateSystem: 'geo',
           z: 10,
           rippleEffect: {
-            brushType: "stroke"
+            brushType: 'stroke'
           },
           symbolSize: 8 / 192 * getBaseFontSize(),
           label: {
             normal: {
               show: true,
-              position: "top",
-              formatter: "{b}",
+              position: 'top',
+              formatter: '{b}',
               textStyle: {
-                color: "#fff",
+                color: '#fff',
                 fontSize: 8 / 192 * getBaseFontSize()
               }
             }
           },
           itemStyle: {
             normal: {
-              color: color
+              color
             }
           },
           data: topGeo
         },
         {
-          type: "scatter",
-          coordinateSystem: "geo",
-          symbolSize: function(val) {
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          symbolSize(val) {
             return val[2] / multiple / 192 * getBaseFontSize();
           },
           label: {
             normal: {
               show: false,
-              position: "top",
-              formatter: "{b}",
+              position: 'top',
+              formatter: '{b}',
               textStyle: {
-                color: "#fff",
+                color: '#fff',
                 fontSize: 10 / 192 * getBaseFontSize()
               }
             }
           },
           itemStyle: {
             normal: {
-              color: color
+              color
             }
           },
           data: geo
         }
       ]
     };
-    const colorList = ["#e0ffff", "#006edd"];
+    const colorList = ['#e0ffff', '#006edd'];
     const provinceMap = {
       visualMap: {
         min: Math.min.apply(null, provinceValue.map(i => i.value)),
@@ -223,8 +219,8 @@ export default class InnerScatterFour extends Component {
       },
       series: [
         {
-          type: "map",
-          mapType: "china",
+          type: 'map',
+          mapType: 'china',
           label: {
             emphasis: {
               show: true
@@ -233,11 +229,11 @@ export default class InnerScatterFour extends Component {
           roam: false,
           itemStyle: {
             normal: {
-              areaColor: "#eeeeee",
-              borderColor: "#666"
+              areaColor: '#eeeeee',
+              borderColor: '#666'
             },
             emphasis: {
-              areaColor: "yellow"
+              areaColor: 'yellow'
             }
           },
           data: provinceValue
@@ -247,9 +243,9 @@ export default class InnerScatterFour extends Component {
     const areaMap = {
       tooltip: {
         show: false,
-        trigger: "item",
-        formatter: params => {
-          return `${params["data"]["area"]}: ${params["data"]["value"]}`;
+        trigger: 'item',
+        formatter: (params) => {
+          return `${params.data.area}: ${params.data.value}`;
         }
       },
       visualMap: {
@@ -264,8 +260,8 @@ export default class InnerScatterFour extends Component {
       },
       series: [
         {
-          type: "map",
-          mapType: "china",
+          type: 'map',
+          mapType: 'china',
           label: {
             emphasis: {
               show: false
@@ -274,11 +270,11 @@ export default class InnerScatterFour extends Component {
           roam: false,
           itemStyle: {
             normal: {
-              areaColor: "#eeeeee",
-              borderColor: "#666"
+              areaColor: '#eeeeee',
+              borderColor: '#666'
             },
             emphasis: {
-              areaColor: "yellow"
+              areaColor: 'yellow'
             }
           },
           data: proArea.map(item => ({
@@ -307,7 +303,7 @@ export default class InnerScatterFour extends Component {
       default:
         data = scatterMap;
     }
-    data["index"] = index;
+    data.index = index;
     return <WDMapBasic key={index} optionCustom={data} className="map" />;
   }
 
@@ -323,15 +319,11 @@ export default class InnerScatterFour extends Component {
             </div>
           </div>
           {showType.map((item, i) =>
-            <div
-              key={i}
-              className={this.state.index === i ? "box-active" : "box"}>
+            <div key={i} className={this.state.index === i ? 'box-active' : 'box'}>
               <img
                 className="box-pic"
                 src={
-                  this.state.index === i
-                    ? "/img/pillar-bg-active.png"
-                    : "/img/pillar-bg-normal.png"
+                  this.state.index === i ? '/img/pillar-bg-active.png' : '/img/pillar-bg-normal.png'
                 }
               />
               <div className="bar-title">
@@ -344,23 +336,20 @@ export default class InnerScatterFour extends Component {
                     className="erea"
                     style={{
                       height: item.percent
-                    }}>
+                    }}
+                  >
                     {pillar.map((item, j) =>
                       <div
                         key={j}
-                        className={
-                          this.state.index === i
-                            ? "pillar pillar-active"
-                            : "pillar"
-                        }
+                        className={this.state.index === i ? 'pillar pillar-active' : 'pillar'}
                       />
                     )}
                   </div>
                 )}
               </div>
-              <div className={i === 0 ? "name-wrap-rotate" : "name-wrap"}>
+              <div className={i === 0 ? 'name-wrap-rotate' : 'name-wrap'}>
                 {pillarData[i].map((item, k) =>
-                  <div key={k} className={i === 0 ? "name-rotate" : "name"}>
+                  <div key={k} className={i === 0 ? 'name-rotate' : 'name'}>
                     {item.name.substring(0, 4)}
                   </div>
                 )}
--- src/containers/InnerScatter/index.js
+++ src/containers/InnerScatter/index.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Echarts from 'echarts-for-react';
-import {WDMapBasic, BgAnimation} from '../../components';
-import {plazaGeo} from '../../assets/map/wdplaza.geo';
-import {cityGeo} from '../../assets/map/city.geo';
-import {provinceValue, proArea, areaValue} from '../../assets/map/mapAreaValue';
+import { WDMapBasic, BgAnimation } from '../../components';
+import { plazaGeo } from '../../assets/map/wdplaza.geo';
+import { cityGeo } from '../../assets/map/city.geo';
+import { provinceValue, proArea, areaValue } from '../../assets/map/mapAreaValue';
 import HeaderTitle from '../Layout/HeaderTitle';
 import { getBaseFontSize } from '../../utils';
 import './innerScatter.less';
@@ -11,107 +11,122 @@ import './innerScatter.less';
 const pillarData = [
   [
     {
-      "name": "郑州惠济万达广场",
+      name: '郑州惠济万达广场',
       value: [113.640612525, 34.8684219261, 13478],
       percent: '100%'
-    }, {
-      "name": "徐州铜山万达广场",
+    },
+    {
+      name: '徐州铜山万达广场',
       value: [117.1902587966, 34.1960255201, 11544],
       percent: '85.7%'
-    }, {
-      "name": "北京槐房万达广场",
+    },
+    {
+      name: '北京槐房万达广场',
       value: [116.3682659401, 39.813840514, 11048],
       percent: '82.0%'
-    }, {
-      "name": "东莞虎门万达广场",
+    },
+    {
+      name: '东莞虎门万达广场',
       value: [113.682834261, 22.8330205339, 10984],
       percent: '81.5%'
-    }, {
-      "name": "烟台开发区万达广场",
+    },
+    {
+      name: '烟台开发区万达广场',
       value: [121.2565552877, 37.5375066159, 10266],
       percent: '76.2%'
     }
   ],
   [
     {
-      "name": "北京",
+      name: '北京',
       value: [116.395645038, 39.9299857781, 338700],
       percent: '100%'
-    }, {
-      "name": "郑州",
+    },
+    {
+      name: '郑州',
       value: [113.64964385, 34.7566100641, 234991],
       percent: '69.4%'
-    }, {
-      "name": "成都",
+    },
+    {
+      name: '成都',
       value: [104.067923463, 30.6799428454, 226535],
       percent: '66.9%'
-    }, {
-      "name": "徐州",
+    },
+    {
+      name: '徐州',
       value: [117.188106623, 34.2715534311, 195691],
       percent: '57.8%'
-    }, {
-      "name": "烟台",
+    },
+    {
+      name: '烟台',
       value: [121.30955503, 37.5365615629, 157102],
       percent: '46.4%'
     }
   ],
   [
     {
-      'name': '江苏',
+      name: '江苏',
       value: 36786,
       percent: '100%'
-    }, {
-      'name': '福建',
+    },
+    {
+      name: '福建',
       value: 26579,
       percent: '72.3%'
-    }, {
-      'name': '北京',
+    },
+    {
+      name: '北京',
       value: 20867,
       percent: '56.7%'
-    }, {
-      'name': '湖北',
+    },
+    {
+      name: '湖北',
       value: 18358,
       percent: '49.9%'
-    }, {
-      'name': '安徽',
+    },
+    {
+      name: '安徽',
       value: 17239,
       percent: '46.9%'
     }
   ],
   [
     {
-      'name': '华东区',
+      name: '华东区',
       value: 119331,
       percent: '100%'
-    }, {
-      'name': '中南区',
+    },
+    {
+      name: '中南区',
       value: 54783,
       percent: '45.9%'
-    }, {
-      'name': '东北区',
+    },
+    {
+      name: '东北区',
       value: 32421,
       percent: '27.2%'
-    }, {
-      'name': '华北区',
+    },
+    {
+      name: '华北区',
       value: 31823,
       percent: '26.7%'
-    }, {
-      'name': '西南区',
+    },
+    {
+      name: '西南区',
       value: 15345,
       percent: '12.9%'
     }
   ]
 ];
 
-const pillar = [0,1,2,3,4,5,6,7,8,9];
+const pillar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const showType = ['广场', '城市', '省份', '区域'];
 export default class InnerScatter extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      index:0
-    }
+      index: 0
+    };
   }
 
   componentDidMount() {
@@ -120,22 +135,22 @@ export default class InnerScatter extends Component {
 
   slideBtn() {
     clearInterval(timer);
-    let ul = document.getElementById('slideWrap');
-    let allLI = ul.getElementsByTagName("li");
-    let selectTitle = document.querySelector('.bar-title');
+    const ul = document.getElementById('slideWrap');
+    const allLI = ul.getElementsByTagName('li');
+    const selectTitle = document.querySelector('.bar-title');
     selectTitle.innerHTML = showType[this.state.index];
 
     let timer = setInterval(() => {
-      if(this.state.index === 3){
-        this.setState({index:0});
-      }else{
-        this.setState({index:this.state.index+1});
+      if (this.state.index === 3) {
+        this.setState({ index: 0 });
+      } else {
+        this.setState({ index: this.state.index + 1 });
       }
       selectTitle.innerHTML = showType[this.state.index];
-    }, 3000)
+    }, 3000);
   }
 
-  renderMap(index){
+  renderMap(index) {
     const topGeo = index === 0 ? pillarData[0] : pillarData[1];
     const geo = index === 0 ? plazaGeo : cityGeo;
     const multiple = index === 0 ? 1000 : 10000;
@@ -149,7 +164,7 @@ export default class InnerScatter extends Component {
           rippleEffect: {
             brushType: 'stroke'
           },
-          symbolSize:8/192*getBaseFontSize(),
+          symbolSize: 8 / 192 * getBaseFontSize(),
           label: {
             normal: {
               show: true,
@@ -157,22 +172,22 @@ export default class InnerScatter extends Component {
               formatter: '{b}',
               textStyle: {
                 color: '#fff',
-                fontSize:10/192*getBaseFontSize()
+                fontSize: 10 / 192 * getBaseFontSize()
               }
             }
           },
           itemStyle: {
             normal: {
-              color: color,
+              color
             }
           },
-          data:  topGeo
+          data: topGeo
         },
         {
           type: 'scatter',
           coordinateSystem: 'geo',
-          symbolSize:function (val) {
-            return val[2]/multiple/192*getBaseFontSize();
+          symbolSize(val) {
+            return val[2] / multiple / 192 * getBaseFontSize();
           },
           label: {
             normal: {
@@ -181,24 +196,34 @@ export default class InnerScatter extends Component {
               formatter: '{b}',
               textStyle: {
                 color: '#fff',
-                fontSize:10/192*getBaseFontSize()
+                fontSize: 10 / 192 * getBaseFontSize()
               }
             }
           },
           itemStyle: {
             normal: {
-              color: color,
+              color
             }
           },
-          data:  geo
+          data: geo
         }
       ]
     };
     const colorList = ['#e0ffff', '#006edd'];
     const provinceMap = {
       visualMap: {
-        min: Math.min.apply(null, provinceValue.map(i=>{return i.value})),
-        max: Math.max.apply(null, provinceValue.map(i=>{return i.value})),
+        min: Math.min.apply(
+          null,
+          provinceValue.map((i) => {
+            return i.value;
+          })
+        ),
+        max: Math.max.apply(
+          null,
+          provinceValue.map((i) => {
+            return i.value;
+          })
+        ),
         show: false,
         inRange: {
           color: colorList
@@ -231,11 +256,11 @@ export default class InnerScatter extends Component {
     };
     const areaMap = {
       tooltip: {
-        show:false,
+        show: false,
         trigger: 'item',
-        formatter:(params)=>{
-          return `${params['data']['area']}: ${params['data']['value']}`
-        },
+        formatter: (params) => {
+          return `${params.data.area}: ${params.data.value}`;
+        }
       },
       visualMap: {
         min: Object.values(areaValue).sort((prev, next) => prev - next)[0],
@@ -266,12 +291,12 @@ export default class InnerScatter extends Component {
               areaColor: 'yellow'
             }
           },
-          data: proArea.map((item)=>{
+          data: proArea.map((item) => {
             return {
-              name:item.name,
-              value:areaValue[item.area],
-              area:item.area
-            }
+              name: item.name,
+              value: areaValue[item.area],
+              area: item.area
+            };
           })
         }
       ]
@@ -294,15 +319,13 @@ export default class InnerScatter extends Component {
       default:
         data = scatterMap;
     }
-    data['index'] = index;
-    return(
-      <WDMapBasic key={index} optionCustom={data} className="map"/>
-    )
+    data.index = index;
+    return <WDMapBasic key={index} optionCustom={data} className="map" />;
   }
   render() {
     return (
       <div>
-        <HeaderTitle title="数字商业：内场分布 + 指标排名" className="sum-title"/>
+        <HeaderTitle title="数字商业：内场分布 + 指标排名" className="sum-title" />
         <BgAnimation />
         <div className="inner-scatter-container">
           <div>
@@ -311,34 +334,40 @@ export default class InnerScatter extends Component {
                 {this.renderMap(this.state.index)}
               </div>
               <div className="bar">
-                <div className="bar-title"></div>
-                <img src="/img/pillar-bg.png" className="pillar-bg"/>
+                <div className="bar-title" />
+                <img src="/img/pillar-bg.png" className="pillar-bg" />
                 <div className="pillar-list">
-                  <div className="erea-wrap" >
-                    {pillarData[this.state.index].map((item, i) => (
-                      <div key={i} className="erea" style={{height: item.percent}}>
+                  <div className="erea-wrap">
+                    {pillarData[this.state.index].map((item, i) =>
+                      <div key={i} className="erea" style={{ height: item.percent }}>
                         <div key={this.state.index} className="child-item">
-                          {pillar.map((item,i) => (
-                            <div key={i} className="pillar"/>
-                          ))}
-                          <div className={this.state.index === 0 ? "name name-rotate" : "name"}>{item.name.substring(0,4)}</div>
+                          {pillar.map((item, i) => <div key={i} className="pillar" />)}
+                          <div className={this.state.index === 0 ? 'name name-rotate' : 'name'}>
+                            {item.name.substring(0, 4)}
+                          </div>
                         </div>
                       </div>
-                    ))}
+                    )}
                   </div>
-               </div>
+                </div>
               </div>
             </div>
-           <div className="slide-btn">
-            <ul id="slideWrap" className="slide-wrap">
-              {showType.map((item, i) => (
-                <li key={i} className={i === this.state.index ? "active": ""} onClick={this.alertMsg}>{item}</li>
-              ))}
-            </ul>
+            <div className="slide-btn">
+              <ul id="slideWrap" className="slide-wrap">
+                {showType.map((item, i) =>
+                  <li
+                    key={i}
+                    className={i === this.state.index ? 'active' : ''}
+                    onClick={this.alertMsg}
+                  >
+                    {item}
+                  </li>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
