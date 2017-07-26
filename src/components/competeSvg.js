a9b71ac969c267ab5125c2b8b51c68445d69bbd1
caozhiwei <13921742327@163.com>
2017-07-26 16:56:35 +0800

    bug

--- src/components/competeSvg.js
+++ src/components/competeSvg.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 
 export default class competeSvg extends Component{
 
-    constu
-
     render() {
         return (
           <svg width="100%" height="100%" viewBox="0 0 280 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
