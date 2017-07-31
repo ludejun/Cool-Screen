ef6527293ffad611092c4bf2d58df3d678ad4189
guming <1065316432@qq.com>
2017-07-31 19:43:06 +0800

    fix ui

--- src/containers/DigitalFinance/index.js
+++ src/containers/DigitalFinance/index.js
@@ -7,17 +7,18 @@ import { getBaseFontSize } from '../../utils';
 import ReactEcharts from 'echarts-for-react';
 import './digitalFinance.less';
 
+// {"tag":["亲子","户外运动品牌店","数码"，"电影","海鲜"，"日料", "西餐", "停车","运动健身", "国际酒店"],'caifu':[4.73,4.16，6.97，0.78，2.13，6.52，11.52，2.29，2.26，0.02],'xiaodai':[16.38,17.29，29.47，2.76，7.26，21.18，49.51，8.03，7.26，0.07]},
 const data = [
-  { name: ' 亲子', img: '/img/doll.png' },
+  { name: '亲子', img: '/img/doll.png' },
   { name: '户外运动品牌店', img: '/img/outdoor.png' },
-  { name: '数码品牌店', img: '/img/camera.png' },
-  { name: '逛电影院', img: '/img/movie.png' },
-  { name: '海鲜店', img: '/img/seafood.png' },
-  { name: '日料店', img: '/img/food.png' },
-  { name: '西餐店', img: '/img/westfood.png' },
-  { name: '进出停车场', img: '/img/park.png' },
-  { name: '进出国际酒店', img: '/img/hotel.png' },
-  { name: '运动健身', img: '/img/run.png' }
+  { name: '数码', img: '/img/camera.png' },
+  { name: '电影', img: '/img/movie.png' },
+  { name: '海鲜', img: '/img/seafood.png' },
+  { name: '日料', img: '/img/food.png' },
+  { name: '西餐', img: '/img/westfood.png' },
+  { name: '停车', img: '/img/park.png' },
+  { name: '运动健身', img: '/img/run.png' },
+  { name: '国际酒店', img: '/img/hotel.png' }
 ];
 export default class DigitalFinance extends Component {
   constructor(props) {
@@ -55,28 +56,28 @@ export default class DigitalFinance extends Component {
       29
     ];
     const data1 = [
-      [1, 0, 5],
-      [4, 0, 1],
-      [7, 0, 3],
-      [10, 0, 4],
-      [13, 0, 5],
-      [16, 0, 6],
-      [19, 0, 7],
-      [22, 0, 8],
-      [25, 0, 7],
-      [28, 0, 8]
+      [1, 0, 4.73],
+      [4, 0, 4.16],
+      [7, 0, 6.97],
+      [10, 0, 0.78],
+      [13, 0, 2.13],
+      [16, 0, 6.52],
+      [19, 0, 11.52],
+      [22, 0, 2.29],
+      [25, 0, 2.26],
+      [28, 0, 0.02]
     ];
     const data2 = [
-      [2, 0, 1],
-      [5, 0, 3],
-      [8, 0, 4],
-      [11, 0, 5],
-      [14, 0, 6],
-      [17, 0, 20],
-      [20, 0, 9],
-      [23, 0, 8],
-      [26, 0, 9],
-      [29, 0, 8]
+      [2, 0, 16.38],
+      [5, 0, 17.29],
+      [8, 0, 29.47],
+      [11, 0, 2.76],
+      [14, 0, 7.26],
+      [17, 0, 21.18],
+      [20, 0, 49.51],
+      [23, 0, 8.03],
+      [26, 0, 7.26],
+      [29, 0, 0.07]
     ];
     this.option = {
       tooltip: {},
@@ -106,6 +107,9 @@ export default class DigitalFinance extends Component {
       zAxis3D: {
         name: '',
         type: 'value',
+        axisLabel: {
+          formatter: '{value}% '
+        },
         axisLine: {
           lineStyle: {
             color: '#0C55B7'
@@ -126,11 +130,11 @@ export default class DigitalFinance extends Component {
         //   [
         //     {
         //       offset: 0,
-        //       color: 'rgba(65,177,255,1)'
+        //       color: 'rgba(65,177,255,0)'
         //     },
         //     {
         //       offset: 1,
-        //       color: 'rgba(23,84,243,0.91)'
+        //       color: 'rgba(23,84,244,0.21)'
         //     }
         //   ],
         //   false
@@ -147,7 +151,8 @@ export default class DigitalFinance extends Component {
           main: {
             intensity: 1.2,
             shadow: true,
-            alpha: 50
+            alpha: 0,
+            beta: 0
           },
           ambient: {
             intensity: 0.3
@@ -191,7 +196,110 @@ export default class DigitalFinance extends Component {
         <HeaderTitle title="数字金融-财富 / 小贷客群的万达生态描绘" className="sum-title" />
         <WDLogoSvg className="header-logo" />
         {/* background */}
-        <BgGradualHeight className="bg-svg" />
+        <BgGradualHeight className="bg-svg" show />
+        {/* circle */}
+        <svg
+          className="circle-top"
+          viewBox="0 0 30 30"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <defs>
+            <ellipse id="path-1" cx="20" cy="40" rx="5" ry="5" />
+            <filter
+              x="-150.0%"
+              y="-150.0%"
+              width="400.0%"
+              height="400.0%"
+              filterUnits="objectBoundingBox"
+              id="filter-2"
+            >
+              <feOffset dx="0" dy="0" in="SourceAlpha" result="shadowOffsetOuter1" />
+              <feGaussianBlur stdDeviation="5" in="shadowOffsetOuter1" result="shadowBlurOuter1" />
+              <feColorMatrix
+                values="0 0 0 0 0.15692077   0 0 0 0 0.886739866   0 0 0 0 1  0 0 0 0.786769701 0"
+                type="matrix"
+                in="shadowBlurOuter1"
+              />
+            </filter>
+          </defs>
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-152.000000, -265.000000)">
+              <g transform="translate(147.000000, 240.000000)">
+                <g id="Oval-3-Copy">
+                  <use fill="black" fillOpacity="1" filter="url(#filter-2)" xlinkHref="#path-1" />
+                  <use fill="#2389F2" fillRule="evenodd" xlinkHref="#path-1" />
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <svg
+          className="circle-bottom-inner"
+          viewBox="0 0 34 34"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <defs>
+            <ellipse id="circle2-1" cx="20" cy="615" rx="7" ry="7" />
+            <filter
+              x="-107.1%"
+              y="-107.1%"
+              width="314.3%"
+              height="314.3%"
+              filterUnits="objectBoundingBox"
+              id="circle2-2"
+            >
+              <feOffset dx="0" dy="0" in="SourceAlpha" result="shadowOffsetOuter1" />
+              <feGaussianBlur stdDeviation="5" in="shadowOffsetOuter1" result="shadowBlurOuter1" />
+              <feColorMatrix
+                values="0 0 0 0 0.15692077   0 0 0 0 0.886739866   0 0 0 0 1  0 0 0 0.786769701 0"
+                type="matrix"
+                in="shadowBlurOuter1"
+              />
+            </filter>
+          </defs>
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-150.000000, -838.000000)">
+              <g transform="translate(147.000000, 240.000000)">
+                <g>
+                  <use
+                    fill="black"
+                    fillOpacity="1"
+                    filter="url(#circle2-2)"
+                    xlinkHref="#circle2-1"
+                  />
+                  <use fill="#2389F2" fillRule="evenodd" xlinkHref="#circle2-1" />
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <svg
+          className="circle-bottom-out"
+          viewBox="0 0 42 42"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <defs />
+          <g
+            stroke="none"
+            strokeWidth="1"
+            fill="none"
+            fillRule="evenodd"
+            fillOpacity="0.265341938"
+            strokeDasharray="3"
+          >
+            <g transform="translate(-146.000000, -834.000000)" stroke="#2C6CFF" fill="#2F45FF">
+              <g transform="translate(147.000000, 240.000000)">
+                <circle id="Oval-44" cx="20" cy="615" r="20" />
+              </g>
+            </g>
+          </g>
+        </svg>
 
         {/* describe tag*/}
         <svg
@@ -204,10 +312,10 @@ export default class DigitalFinance extends Component {
           xmlnsXlink="http://www.w3.org/1999/xlink"
         >
           <defs />
-          <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g id="数字金融" transform="translate(-1546.000000, -243.000000)">
-              <g id="Group-4" transform="translate(1547.000000, 244.000000)">
-                <g id="Group-2">
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-1546.000000, -243.000000)">
+              <g transform="translate(1547.000000, 244.000000)">
+                <g>
                   <polygon
                     id="Path-864"
                     stroke="#2593E1"
@@ -215,9 +323,15 @@ export default class DigitalFinance extends Component {
                     fill="#112C52"
                     points="19.6350912 4.17013932 0.446552179 23.3586783 0.446552179 67.9156793 4.95903429 72.4281614 4.95903429 89.4414469 0.569316407 93.8311647 0.569316407 126.330959 131.649705 126.330959 149.263105 108.717558 149.263105 72.7734068 143.6081 67.1184011 143.6081 36.0917267 149.171778 30.5280487 149.171778 4.22986773"
                   />
-                  <circle id="Oval-4" fill="#2593E1" cx="19.5" cy="4.5" r="2.5" />
-                  <circle id="Oval-4-Copy-2" stroke="#2593E1" cx="19.5" cy="4.5" r="4.5" />
-                  <circle id="Oval-4-Copy" fill="#2593E1" cx="132.5" cy="125.5" r="2.5" />
+                  <circle className="desc-tag-circle" fill="#2593E1" cx="19.5" cy="4.5" r="2.5" />
+                  <circle className="desc-tag-circle" stroke="#2593E1" cx="19.5" cy="4.5" r="4.5" />
+                  <circle
+                    className="desc-tag-circle"
+                    fill="#2593E1"
+                    cx="132.5"
+                    cy="125.5"
+                    r="2.5"
+                  />
                   <polyline
                     id="Path-870"
                     stroke="#137BF4"
@@ -229,7 +343,7 @@ export default class DigitalFinance extends Component {
                     points="144.048799 76.2637947 144.048799 105.619069 131.492299 118.175568"
                   />
                   <polyline
-                    id="Path-872"
+                    className="desc-tag-top"
                     stroke="#137BF4"
                     strokeDasharray="5,3"
                     points="11.3765294 51.1327502 11.3765294 91.7730403 7.07411894 96.0754508 7.07411894 120.089813 72.0173095 120.089813"
@@ -237,7 +351,7 @@ export default class DigitalFinance extends Component {
                   <circle id="Oval-5" fill="#147BF4" cx="69" cy="120" r="2" />
                   <circle id="Oval-5-Copy" fill="#147BF4" cx="142" cy="10" r="2" />
                   <polyline
-                    id="Path-873"
+                    className="desc-tag-bottom"
                     stroke="#137BF4"
                     strokeDasharray="5,3"
                     points="136.898129 83.8824001 136.898129 35.9662233 142.268015 30.5963377 142.268015 10.2618689 44.9632007 10.2618689"
