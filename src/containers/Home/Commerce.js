67046fb275e2baaddd4e3fb08574cbfe3091bf7d
ludejun1 <ludejun1@wanda.cn>
2017-08-14 14:11:19 +0800

    超大屏数字变化调整

--- src/containers/Home/Commerce.js
+++ src/containers/Home/Commerce.js
@@ -23,19 +23,20 @@ export default class Commerce extends Component {
     ];
     resizeSquarePage();
   }
+
   render() {
     const consume = ['31%', '43%', '26%'];
     const list = [
-      { name: '江苏省', val: 0.058, val1: 0.941, num: 361 },
-      { name: '浙江省', val: 0.057, val1: 0.942, num: 328 },
-      { name: '广东省', val: 0.065, val1: 0.934, num: 307 },
-      { name: '陕西省', val: 0.03, val1: 0.969, num: 194 },
-      { name: '河北省', val: 0.017, val1: 0.982, num: 174 },
-      { name: '福建省', val: 0.076, val1: 0.923, num: 170 },
-      { name: '湖北省', val: 0.069, val1: 0.93, num: 143 },
-      { name: '安徽省', val: 0.12, val1: 0.879, num: 124 },
-      { name: '山东省', val: 0.114, val1: 0.885, num: 122 },
-      { name: '山西省', val: 0.016, val1: 0.983, num: 120 }
+      {name: '江苏省', val: 0.058, val1: 0.941, num: 361},
+      {name: '浙江省', val: 0.057, val1: 0.942, num: 328},
+      {name: '广东省', val: 0.065, val1: 0.934, num: 307},
+      {name: '陕西省', val: 0.03, val1: 0.969, num: 194},
+      {name: '河北省', val: 0.017, val1: 0.982, num: 174},
+      {name: '福建省', val: 0.076, val1: 0.923, num: 170},
+      {name: '湖北省', val: 0.069, val1: 0.93, num: 143},
+      {name: '安徽省', val: 0.12, val1: 0.879, num: 124},
+      {name: '山东省', val: 0.114, val1: 0.885, num: 122},
+      {name: '山西省', val: 0.016, val1: 0.983, num: 120}
     ];
     const genderList = [
       [
@@ -75,7 +76,7 @@ export default class Commerce extends Component {
                     {v.title}
                   </p>
                   <p className="line-num">
-                    <AnimeNumber num={v.num} fromNum={v.num - 100} />
+                    <AnimeNumber num={v.num} fromNum={parseInt(v.num * 0.9, 10)} />
                     <span className="line-unit">
                       {v.unit}
                     </span>
@@ -106,7 +107,7 @@ export default class Commerce extends Component {
                 <p className="sub-title">带动就业</p>
                 <p className="num">
                   <AnimeNumber num={1820000} fromNum={1820000 - 100} />
-                  <span className="unit">人</span>
+                  <span className="unit">+人</span>
                 </p>
               </div>
             </div>
@@ -114,7 +115,7 @@ export default class Commerce extends Component {
 
           <div className="flex1">
             <p className="bar-title">广场分布省市排名Top 10</p>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
               <div className=" light-blue" />
               <div className="legend">万达广场</div>
               <div className=" deep-blue" />
@@ -127,13 +128,13 @@ export default class Commerce extends Component {
                 </div>
                 <div className="flex1">
                   <div className="money-color-container">
-                    <div className="money-color-0" style={{ width: `${item.val * 100}%` }} />
-                    <div className="money-color-1" style={{ width: `${item.val1 * 100}%` }} />
+                    <div className="money-color-0" style={{width: `${item.val * 100}%`}} />
+                    <div className="money-color-1" style={{width: `${item.val1 * 100}%`}} />
                   </div>
                 </div>
                 {/* <div className="money-num">
-                  {item.val}
-                </div> */}
+                 {item.val}
+                 </div> */}
               </div>
             )}
           </div>
