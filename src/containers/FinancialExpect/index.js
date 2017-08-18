e4bdcbea393d85b58bbbe4f5c59b42ea8d3d3b33
ludejun1 <ludejun1@wanda.cn>
2017-08-18 12:05:16 +0800

    投资金融-万达财富style调整，添加动画

--- src/containers/FinancialExpect/index.js
+++ src/containers/FinancialExpect/index.js
@@ -2077,8 +2077,8 @@ class Expect extends Component {
                 <div className="img1"></div>
                 <div className="img2"></div>
               </div>
-              <p>扫码下载万达财富APP</p>
-              <p>注册最高可得8888体验金</p>
+              <p className="financial-ad-text text1">扫码下载万达财富APP</p>
+              <p className="financial-ad-text text2">注册最高可得<span style={{color: 'yellow', fontWeight: '400'}}>88888</span>体验金</p>
             </div>
           </div>
         </div>
